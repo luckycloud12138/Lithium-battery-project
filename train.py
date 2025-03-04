1869bd7d2b8f3782cb16cec5a89bfb545232a7f7
luckycloud12138 <62422996+luckycloud12138@users.noreply.github.com>
2025-03-04 10:39:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,12 +2,6 @@ import warnings
 warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
-# BILIBILI UP 魔傀面具
-# 训练参数官方详解链接：https://docs.ultralytics.com/modes/train/#resuming-interrupted-trainings:~:text=a%20training%20run.-,Train%20Settings,-The%20training%20settings
-
-# 指定显卡和多卡训练问题 统一都在<YOLOV11配置文件.md>下方常见错误和解决方案。
-# 训练过程中loss出现nan，可以尝试关闭AMP，就是把下方amp=False的注释去掉。
-
 if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/models/11/yolo11-C3k2-GhostDynamicConv.yaml.yaml')
     # model.load('yolo11n.pt') # loading pretrain weights
@@ -19,11 +13,7 @@ if __name__ == '__main__':
                 close_mosaic=0,
                 workers=0,
                 # device='0',
-                optimizer='SGD', # using SGD
-                # patience=0, # close earlystop
-                # resume=True, # 断点续训,YOLO初始化时选择last.pt
-                # amp=False, # close amp
-                # fraction=0.2,
+                optimizer='SGD',
                 project='runs/train',
                 name='test',
                 )
